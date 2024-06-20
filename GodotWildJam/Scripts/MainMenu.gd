5343c0593e73085d2d7c13f5a5e724001f0215e9
Deltazoe <138903048+Deltazoe@users.noreply.github.com>
2024-06-20 19:37:34 -0400

    wolf system changed, audio settings added, name space errors fixed

--- GodotWildJam/Scripts/MainMenu.gd
+++ GodotWildJam/Scripts/MainMenu.gd
@@ -2,42 +2,63 @@ extends Node2D
 
 @onready var v_box_container = $VBoxContainer
 @onready var difficulty = $Difficulty
+@onready var options_v_box = $VBoxContainer/Options/VBoxContainer
+@onready var audio_options = $VBoxContainer/Options/VBoxContainer/Audio/HBoxContainer
+@onready var ExitDelay = $VBoxContainer/Quit/ExitDelay
+@onready var PaperTurnForward = $PaperTurnForward
+@onready var PaperTurnBackward = $PaperTurnBackward
+
 var WolfSpeedValue = 0
 
 func _ready():
 	difficulty.set_process(false)
 
+#start
 func _on_start_pressed():
+	PaperTurnForward.play()
 	v_box_container.set_process(false)
 	v_box_container.visible = false
 	difficulty.set_process(true)
 	difficulty.visible = true
 
-
+#options
 func _on_options_pressed():
-	get_tree().change_scene_to_file("res://Scene/Options.tscn")
+	options_v_box.visible = true
+	PaperTurnForward.play()
 
+func _on_audio_pressed():
+	audio_options.visible = true
+	PaperTurnForward.play()
+
+func _on_back_options_pressed():
+	audio_options.visible = false
+	options_v_box.visible = false
+	PaperTurnBackward.play()
+func _on_keybindings_pressed():
+	pass # Replace with function body.
+	PaperTurnForward.play()
 
 func _on_quit_pressed():
-	get_tree().quit()
+	ExitDelay.start()
+	PaperTurnBackward.play()
 
-
+#difficulties
 func _on_easy_pressed():
 	GlobalLogicHandler.WolfSpeedSetter(50)
 	ResetAll()
-	get_tree().change_scene_to_file("res://Scene/Root.tscn")
+	get_tree().change_scene_to_file("res://Scene/root.tscn")
 	GameMusicPersist.Ready2Rock()
 
 func _on_medium_pressed():
 	GlobalLogicHandler.WolfSpeedSetter(100)
 	ResetAll()
-	get_tree().change_scene_to_file("res://Scene/Root.tscn")
+	get_tree().change_scene_to_file("res://Scene/root.tscn")
 	GameMusicPersist.Ready2Rock()
 
 func _on_hard_pressed():
 	GlobalLogicHandler.WolfSpeedSetter(200)
 	ResetAll()
-	get_tree().change_scene_to_file("res://Scene/Root.tscn")
+	get_tree().change_scene_to_file("res://Scene/root.tscn")
 	GameMusicPersist.Ready2Rock()
 	
 func ResetAll():
@@ -45,3 +66,6 @@ func ResetAll():
 	v_box_container.visible = true
 	difficulty.set_process(false)
 	difficulty.visible = false
+
+func _on_exit_delay_timeout():
+	get_tree().quit()
--- GodotWildJam/Scripts/Wolf.gd
+++ GodotWildJam/Scripts/Wolf.gd
@@ -2,7 +2,14 @@ extends CharacterBody2D
 
 signal PlayerKilled
 
+@onready var player = $"../Player"
+
+@export var hunt_distance = 600.0
+@export var hunt_speed = 500
+
 var SPEED = 100.0
+
+
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -11,12 +18,20 @@ func _ready():
 	SPEED = GlobalLogicHandler.WolfSpeedGetter()
 
 func _physics_process(_delta):
-	velocity.x = 1 * SPEED
-	move_and_slide()
+	movement_system()
 
 
 func _on_area_2d_body_entered(body):
 	if body.is_in_group("Player"):
 		queue_free()
 		emit_signal("PlayerKilled")
-	
+
+func movement_system():
+	var player_position = player.global_position.x
+	var wolf_position = global_position.x
+	var distance = player_position - wolf_position
+	if distance > hunt_distance:
+		velocity.x = 1 * hunt_speed
+	else:
+		velocity.x = 1 * SPEED
+	move_and_slide()
